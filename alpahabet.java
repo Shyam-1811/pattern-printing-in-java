802dcdac97df703aa220c1ef20b89f358dfd946a
Shyam-1811 <shyamverma1811@gmail.com>
2024-06-03 01:13:22 +0530

    first commit

---
+++ alpahabet.java
@@ -0,0 +1,13 @@
+public class alpahabet {
+    public static void main(String[] args)
+     {
+         
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 5; j++){
+                System.out.print((char)(j+64)+" " );
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ alphabetSmall.java
@@ -0,0 +1,15 @@
+public class alphabetSmall 
+{
+    public static void main(String[] args) {
+
+    {
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 5; j++){
+                System.out.print((char)(j+96)+" " );
+            }
+            System.out.println();
+        }
+
+    }
+}
+}
